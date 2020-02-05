8c048604c3a1094cfbba472a8c47371243c704ca
Rajesh <rajeshceg3@gmail.com>
2020-02-05 17:48:54 +0530

    Update 961. N-Repeated Element in Size 2N Array.cpp

--- 961. N-Repeated Element in Size 2N Array.cpp
+++ 961. N-Repeated Element in Size 2N Array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& A) {
-		int n = A.size()/2;
-		std::unordered_set<int> hash_set;
-		for(const auto& slice: A)
-		{
-			if( hash_set.count(slice) == 0) hash_set.insert(slice);
-			else return slice;
-		}		
-        return 0;
+	int n = A.size()/2;
+	std::unordered_set<int> hash_set;
+	for(const auto& slice: A)
+	{
+		if( hash_set.count(slice) == 0) hash_set.insert(slice);
+		else return slice;
+	}		
+	return 0;
     }
 };
